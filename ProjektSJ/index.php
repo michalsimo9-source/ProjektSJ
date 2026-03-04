7d1ea5f3914edea01c340c921942abb0978dbeec
michalsimo9-source <michalsimo9@gmail.com>
2026-03-04 12:32:35 +0100

    prvy commit

--- ProjektSJ/index.php
+++ ProjektSJ/index.php
@@ -9,6 +9,7 @@
 <h1>Vitajte na mojej prvej stránke v PHP!</h1>
 <?php
 echo "<h1>Hello world!</h1>";
+echo "<h1>Ahoj svet!</h1>";
 echo "Dnes je " . date("d.m.Y") . "<br>";
 echo "Aktuálny čas je " . date("H:i:s") . "<br>";
 ?>
